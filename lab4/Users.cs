744c066fda03cb76adbb44e05d6986afa90f3a81
FMishka <51850577+FMishka@users.noreply.github.com>
2022-04-24 17:26:58 +0300

    Add files via upload

---
+++ lab4/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supabase;
+using Postgrest.Attributes;
+
+namespace lab4
+{
+    internal class Users : SupabaseModel
+    {
+        [PrimaryKey("id", false)]
+        public int id { get; set; }
+        [Column("login")]
+        public string login { get; set; }
+        [Column("password")]
+        public string password { get; set; }
+    }
+}
